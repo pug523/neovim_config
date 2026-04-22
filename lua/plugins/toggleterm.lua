6277f09dc3cab6fcadb0849e53dfb891712d84b2
pugur <pugurmc@gmail.com>
2026-04-22 22:03:19 +0900

    feat: dynamic term size

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,14 @@
 vim.pack.add({ "https://github.com/akinsho/toggleterm.nvim" })
 require("toggleterm").setup({
-  size = 88,
+  size = function(term)
+    if term.direction == "vertical" then
+      return vim.o.columns * 0.3
+    elseif term.direction == "horizontal" then
+      return 15
+    else
+      return 20
+    end
+  end,
   open_mapping = [[<C-t>]],
   shade_filetypes = {},
   direction = "vertical",
