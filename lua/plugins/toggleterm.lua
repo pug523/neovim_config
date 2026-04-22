3af44c24f59f2e501489085458fc06fabd57f74c
pugur <pugurmc@gmail.com>
2026-04-22 22:00:03 +0900

    refactor: toggle term to vertical

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,2 +1,7 @@
 vim.pack.add({ "https://github.com/akinsho/toggleterm.nvim" })
-require("toggleterm").setup({})
+require("toggleterm").setup({
+  size = 88,
+  open_mapping = [[<C-t>]],
+  shade_filetypes = {},
+  direction = "vertical",
+})
